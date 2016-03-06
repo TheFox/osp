d8a203a0fd7e2a61ed12127d3a6b605d8e41190f
Christian Mayer <christian@fox21.at>
2016-03-06 17:27:37 +0100

    Version output.
    - -V argument added.
    - Version date added.

--- lib/osp/version.rb
+++ lib/osp/version.rb
@@ -2,8 +2,8 @@
 module TheFox
 	module OSP
 		RELEASE_ID = 6
-		VERSION = '0.4.0'
-		DATE = '2016-03-05'
+		VERSION = '0.5.0-dev.1'
+		DATE = '2016-03-06'
 		HOMEPAGE = 'https://github.com/TheFox/osp'
 	end
 end
