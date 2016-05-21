17628b50a10949959842d5748fa8056c8119cee2
Christian Mayer <christian@fox21.at>
2016-05-21 10:35:58 +0200

    Ruby Common Big 2016-04-09 #RN

--- lib/osp/version.rb
+++ lib/osp/version.rb
@@ -2,8 +2,8 @@
 module TheFox
 	module OSP
 		RELEASE_ID = 6
-		VERSION = '0.5.0-dev.2'
-		DATE = '2016-04-09'
+		VERSION = '0.5.0-dev.3'
+		DATE = '2016-05-21'
 		HOMEPAGE = 'https://github.com/TheFox/osp'
 	end
 end
