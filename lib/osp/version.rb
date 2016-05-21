4a86635bda1c8292521024d092501069a90d484c
Christian Mayer <christian@fox21.at>
2016-05-21 10:45:44 +0200

    Version 0.5.0.

--- lib/osp/version.rb
+++ lib/osp/version.rb
@@ -1,8 +1,8 @@
 
 module TheFox
 	module OSP
-		RELEASE_ID = 6
-		VERSION = '0.5.0-dev.4'
+		RELEASE_ID = 7
+		VERSION = '0.5.0'
 		DATE = '2016-05-21'
 		HOMEPAGE = 'https://github.com/TheFox/osp'
 	end
