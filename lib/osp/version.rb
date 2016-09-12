be69eb4fb8efb7676307ee7722f06248693cc675
Christian Mayer <christian@fox21.at>
2016-09-12 23:14:38 +0200

    Test against Ruby 2.3 and HEAD.

--- lib/osp/version.rb
+++ lib/osp/version.rb
@@ -2,8 +2,8 @@
 module TheFox
 	module OSP
 		RELEASE_ID = 7
-		VERSION = '0.6.0-dev.1'
-		DATE = '2016-06-22'
+		VERSION = '0.6.0-dev.2'
+		DATE = '2016-09-12'
 		HOMEPAGE = 'https://github.com/TheFox/osp'
 	end
 end
