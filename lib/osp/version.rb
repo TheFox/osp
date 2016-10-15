d84e27d8b94c952c4bd1d74a62b16f0dfff2eea2
Christian Mayer <christian@fox21.at>
2016-10-15 22:45:55 +0200

    GitLab Deployment added.
    Closes #3.

--- lib/osp/version.rb
+++ lib/osp/version.rb
@@ -2,8 +2,8 @@
 module TheFox
 	module OSP
 		RELEASE_ID = 7
-		VERSION = '0.6.0-dev.4'
-		DATE = '2016-09-12'
+		VERSION = '0.6.0-dev.5'
+		DATE = '2016-10-16'
 		HOMEPAGE = 'https://github.com/TheFox/osp'
 	end
 end
